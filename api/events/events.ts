dea1c186b2a461a4b22500062c64a46373a47c65
David <berghdavid@hotmail.com>
2021-10-04 11:26:02 +0200

    Redesign and refactor events & tickets-API

--- api/events/events.ts
+++ api/events/events.ts
@@ -28,16 +28,11 @@ export interface SingleEvent {
   date: string,
 }
 
-export interface TicketRequest {
-  "event_id": number,
-  "photo": boolean,
-}
-
-export interface Ticket {
-  ticket_code: string,
-  photo: boolean,
-  id: number,
-  event_id: number
+export function fillMissingAttributes(event: SingleEvent) {
+  if(event.date == undefined) {
+    event.date = "asd"
+  }
+  
 }
 
 export const getAllEvents = async (): Promise<ListedEvent[]> => {
@@ -52,24 +47,4 @@ export const getSingleEvent = async (id: number): Promise<SingleEvent> => {
   const json = await response.json();
   const event = <SingleEvent>json.data;
   return event;
-}
-
-export const createTicket = async (TicketRequest: TicketRequest): Promise<Ticket> => {
-  const response = await putAuth('/event/ticket', TicketRequest);
-  console.log(response)
-  const json = await response.json();
-  const ticket = <Ticket>json.data;
-  return ticket;
-}
-
-export const removeTicket = async (eventId: number): Promise<boolean> => {
-  const response = await deleteAuth('/event/ticket/' + eventId, eventId);
-  return response.ok;
-}
-
-export const getAllTickets = async (): Promise<Ticket[]> => {
-  const response = await getAuth('/event/tickets');
-  const json = await response.json();
-  const tickets = <Ticket[]>json.data;
-  return tickets;
 }
