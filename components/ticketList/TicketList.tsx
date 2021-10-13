3961870d78461e681ccba1be0e144a7a4c34b50c
David <berghdavid@hotmail.com>
2021-10-13 15:46:08 +0200

    Create a basic screen for booked tickets
    Creates a new screen, TicketsScreen, which shows all the booked tickets
    for this user.
    The screen is still pretty ugly and needs more UI/UX content.
    
    Separated the TicketItem type from TicketList to solve CI-errors.

---
+++ components/ticketList/TicketList.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Dimensions, FlatList, StyleSheet, View } from 'react-native';
+
+import { TicketListItem } from './TicketListItem';
+import { TicketItem } from './TicketItem';
+
+type TicketListProps = {
+  ticketItems: TicketItem[] | null;
+  onPress: (ticketItem: TicketItem) => void;
+}
+
+const { width, height } = Dimensions.get('window')
+
+export const TicketList = ({ ticketItems, onPress }: TicketListProps) => 
+  <FlatList
+    showsVerticalScrollIndicator={false}
+    data={ticketItems}
+    keyExtractor={({ ticket }) => ticket.id.toString()}
+    renderItem={({ item: ticketItem }) => 
+      <View style={styles.eventBox}>
+        <TicketListItem
+          ticketItem={ticketItem}
+          itemStyle={{}}
+          onPress={() => onPress(ticketItem)} />
+      </View>
+    }
+  />
+
+  const styles = StyleSheet.create({
+    eventBox: {
+      width: width * 0.85,
+      height: height * 0.24
+    },
+  });
---
+++ screens/TicketsScreen.tsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+
+import { View } from '../components/Themed';
+
+import ScreenActivityIndicator from '../components/ScreenActivityIndicator';
+import { ArkadText } from '../components/StyledText';
+
+import { Ticket } from '../api/tickets';
+import { TicketList } from '../components/ticketList/ticketList';
+import { API } from '../api';
+import { TicketItem } from '../components/ticketList/TicketItem';
+
+type TicketsScreenParams = {
+  route: {
+    params: {
+      tickets: Ticket[];
+    };
+  };
+}
+
+export default function ProfileScreen({ route }: TicketsScreenParams) {
+  const { tickets } = route.params;
+  const [loading, setLoading] = useState<boolean>(false);
+  const [ticketItems, setTicketItems] = useState<TicketItem[]>([]);
+
+  useEffect(() => {
+    setLoading(true);
+    loadTickets();
+    setLoading(false);
+  }, []);
+
+  const loadTickets = async () => {
+    const events = await API.events.getBookedEvents();
+    const ticketItems: TicketItem[] = [];
+    for (let i1 = 0; i1 < tickets.length; i1++) {
+      for(let i2 = 0; i2 < events.length; i2++) {
+        if(tickets[i1].eventId == events[i2].id) {
+          const ticketItem: TicketItem = {
+            ticket: tickets[i1],
+            event: events[i2]
+          }
+          ticketItems.push(ticketItem);
+          break;
+        }
+      }
+    }
+    setTicketItems(ticketItems);
+  }
+
+  const viewTicket = (ticketItem: TicketItem) => {
+    // TODO: When pressing a ticket
+    // Open the event in EventDetailsScreen perhaps?
+  }
+
+
+  if (loading || ticketItems == undefined) {
+    return (
+      <View style={styles.container}>
+        <ScreenActivityIndicator />
+        <ArkadText text='Loading tickets...' style={{}} />
+      </View>
+    );
+  }
+  else {
+    return (
+      <View style={styles.container}>
+        <TicketList
+          ticketItems={ticketItems}
+          onPress={viewTicket} />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
