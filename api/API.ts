76800850c906820b2813641438365c74920a4349
Erik Marforio <emarforio@gmail.com>
2021-10-16 11:18:08 +0200

    Add all User methods

--- api/API.ts
+++ api/API.ts
@@ -1,15 +1,15 @@
 import * as Auth from './auth';
 import * as Companies from './companies';
-import * as events from './events';
-import * as tickets from './tickets';
+import * as Events from './events';
+import * as Tickets from './tickets';
 import * as Users from './users';
 import * as SignUp from './signup';
 
 export class API {
   static auth = Auth
   static companies = Companies
-  static events = events
-  static tickets = tickets
+  static events = Events
+  static tickets = Tickets
   static users = Users
   static signup = SignUp
 }
