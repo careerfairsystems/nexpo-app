437b1a74dfa5e0838be9a73f1dadc043ab2353c1
Cecilia <cecryi@gmail.com>
2021-11-16 21:47:57 +0100

    trying keyboardAwareScrollView

--- components/profileScreen/EditStudentProfile.tsx
+++ components/profileScreen/EditStudentProfile.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import { UpdateStudentDto, Student, Guild } from '../../api/students';
-import { View, Text } from '../Themed';
-import { StyleSheet } from 'react-native';
-import { TextInput } from '../TextInput';
-import { EditStatus } from '../../screens/EditProfileScreen';
-import { Picker } from '@react-native-picker/picker';
-import Colors from '../../constants/Colors';
+import React from "react";
+import { UpdateStudentDto, Student, Guild } from "../../api/students";
+import { View, Text } from "../Themed";
+import { StyleSheet } from "react-native";
+import { TextInput } from "../TextInput";
+import { EditStatus } from "../../screens/EditProfileScreen";
+import { Picker } from "@react-native-picker/picker";
+import Colors from "../../constants/Colors";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 type EditStudentProfileProps = {
   student: Student;
   setUpdateStudentDto: (dto: UpdateStudentDto) => void;
   setEditStatus: (status: EditStatus) => void;
-}
+};
 
-export default function EditStudentProfile({ student, setUpdateStudentDto, setEditStatus }: EditStudentProfileProps) {
+export default function EditStudentProfile({
+  student,
+  setUpdateStudentDto,
+  setEditStatus,
+}: EditStudentProfileProps) {
   const [guild, setGuild] = React.useState<Guild | null>(student.guild);
   const [year, setYear] = React.useState<number | null>(student.year);
-  const [masterTitle, setMasterTitle] = React.useState<string | null>(student.masterTitle);
-  const [linkedIn, setLinkedIn] = React.useState<string | null>(student.linkedIn);
+  const [masterTitle, setMasterTitle] = React.useState<string | null>(
+    student.masterTitle
+  );
+  const [linkedIn, setLinkedIn] = React.useState<string | null>(
+    student.linkedIn
+  );
 
   React.useEffect(() => {
     const dto = {
@@ -25,70 +34,82 @@ export default function EditStudentProfile({ student, setUpdateStudentDto, setEd
       year,
       masterTitle,
       linkedIn,
-    }
+    };
     setUpdateStudentDto(dto);
+  }, [guild, linkedIn, masterTitle, year]);
 
-  }, [guild, linkedIn, masterTitle, year])
+  return (
+    <KeyboardAwareScrollView>
+      <View style={styles.container}>
+        <Text>Guild</Text>
+        <Picker
+          style={styles.picker}
+          selectedValue={guild}
+          onValueChange={(value, index) => {
+            if (index === 0) setGuild(null);
+            else setGuild(Number(value));
+          }}
+        >
+          <Picker.Item label="Select your guild" />
+          {Object.keys(Guild)
+            .map(Number)
+            .filter((key) => !isNaN(key))
+            .map((guild) => (
+              <Picker.Item label={Guild[guild]} value={guild} />
+            ))}
+        </Picker>
 
-  return <>
-    <View style={styles.container}>
-      <Text>Guild</Text>
-      <Picker
-        style={styles.picker}
-        selectedValue={guild}
-        onValueChange={(value, index) => {
-          if (index === 0) setGuild(null);
-          else setGuild(Number(value));
-        }}>
-        <Picker.Item label="Select your guild"/>
-        {Object.keys(Guild).map(Number).filter(key => !isNaN(key)).map(guild =>
-          <Picker.Item label={Guild[guild]} value={guild} />
-        )}
-      </Picker>
+        <Text>Year</Text>
+        <Picker
+          style={styles.picker}
+          selectedValue={year}
+          onValueChange={(value, index) => {
+            if (index === 0) setYear(null);
+            else setYear(Number(value));
+          }}
+        >
+          <Picker.Item label="Select a year" />
+          <Picker.Item label="1" value={1} />
+          <Picker.Item label="2" value={2} />
+          <Picker.Item label="3" value={3} />
+          <Picker.Item label="4" value={4} />
+          <Picker.Item label="5" value={5} />
+        </Picker>
 
-      <Text>Year</Text>
-      <Picker
-        style={styles.picker}
-        selectedValue={year}
-        onValueChange={(value, index) => {
-          if (index === 0) setYear(null);
-          else setYear(Number(value));
-        }}>
-        <Picker.Item label="Select a year"/>
-        <Picker.Item label="1" value={1} />
-        <Picker.Item label="2" value={2} />
-        <Picker.Item label="3" value={3} />
-        <Picker.Item label="4" value={4} />
-        <Picker.Item label="5" value={5} />
-      </Picker>
+        <Text>Master Title</Text>
+        <TextInput
+          style={styles.textInput}
+          value={masterTitle ? masterTitle : ""}
+          onChangeText={setMasterTitle}
+        />
 
-      <Text>Master Title</Text>
-      <TextInput style={styles.textInput}
-        value={masterTitle ? masterTitle : ''} onChangeText={setMasterTitle}/>
-
-      <Text>LinkedIn</Text>
-      <TextInput style={styles.textInput}
-        value={linkedIn ? linkedIn : ''} onChangeText={setLinkedIn}/>
-    </View>
-  </>;
+        <Text>LinkedIn</Text>
+        <TextInput
+          style={styles.textInput}
+          value={linkedIn ? linkedIn : ""}
+          onChangeText={setLinkedIn}
+        />
+      </View>
+    </KeyboardAwareScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
   },
   picker: {
-    width: '80%',
+    width: "80%",
     maxWidth: 400,
     padding: 10,
     borderRadius: 3,
     borderColor: Colors.gray,
-    fontFamily: 'montserrat',
+    fontFamily: "montserrat",
     margin: 12,
   },
   textInput: {
-    width: '80%',
+    width: "80%",
     maxWidth: 400,
   },
 });
