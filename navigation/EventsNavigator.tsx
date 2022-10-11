484e2651e02677da1c08f7356c31a1fb2ffd56ae
Jonatan Kruse <jonatankruse@hotmail.com>
2022-10-11 16:26:12 +0200

    cleanup from merge

--- navigation/EventsNavigator.tsx
+++ navigation/EventsNavigator.tsx
@@ -2,14 +2,20 @@ import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
 import EventDetailsScreen from '../screens/EventDetailsScreen';
 import EventListScreen from "../screens/EventListScreen";
+import EventParticipantsScreen from '../screens/EventParticipantsScreen';
+import QRScreen from '../screens/QRScreen';
 
 
 export type EventStackParamlist = {
   EventListScreen: undefined;
+  QRScreen: undefined;
+  EventParticipantsScreen: {
+    eventId: number;
+  }
   EventDetailsScreen: {
     id: number;
-  };
-};
+  }
+}
 const EventStack = createStackNavigator<EventStackParamlist>();
 export function EventsNavigator() {
   return (
@@ -17,11 +23,23 @@ export function EventsNavigator() {
       <EventStack.Screen
         name="EventListScreen"
         component={EventListScreen}
-        options={{ title: 'Events', headerTitle: 'Events' }} />
+        options={{ title: 'Events', headerTitle: 'Events' }}
+      />
       <EventStack.Screen
         name="EventDetailsScreen"
         component={EventDetailsScreen}
-        options={{ title: 'Event Details', headerTitle: 'Event Details' }} />
+        options={{ title: 'Event Details', headerTitle: 'Event Details' }}
+      />
+      <EventStack.Screen
+        name="EventParticipantsScreen"
+        component={EventParticipantsScreen}
+        options={{ title: 'Event Participants', headerTitle: 'Event Participants' }}
+      />
+      <EventStack.Screen
+        name="QRScreen"
+        component={QRScreen}
+        options={{ title: 'QR Scan', headerTitle: 'QR Scan' }}
+      />
     </EventStack.Navigator>
   );
 }
