1d3a380beb21dba1dca395295ab3200c796d2315
Hampus Mattfolk <hampus.mattfolk.1150@student.lu.se>
2022-11-02 01:13:26 +0100

    in progress

--- app.config.ts
+++ app.config.ts
@@ -22,6 +22,7 @@ export default {
     buildNumber: '1.2.1',
     supportsTablet: true,
     infoPlist: {
+      NSAppTransportSecurity: 'NSAllowsArbitraryLoads',
       NSCameraUsageDescription: 'Camera access is needed for the QR-code scanning functionality',
       NSPhotoLibraryUsageDescription: 'Photo library access is needed to choose a profile picture to upload',
     }
@@ -41,9 +42,9 @@ export default {
   extra: {
     isDev: process.env.NODE_ENV === 'development',
     isProd: process.env.NODE_ENV !== 'development',
-    backendUrl: process.env.NODE_ENV === 'development' ? (process.env.BACKEND_URL ? process.env.BACKEND_URL : 'https://nexpo-dev.arkadtlth.se/api') : 'https://nexpo.arkadtlth.se/api',  
-    // backendUrl: 'http://localhost/api',
-    // backendUrl: 'https://nexpo.arkadtlth.se/api',
+    //backendUrl: process.env.NODE_ENV === 'development' ? (process.env.BACKEND_URL ? process.env.BACKEND_URL : 'https://nexpo-dev.arkadtlth.se/api') : 'https://nexpo.arkadtlth.se/api',  
+    //backendUrl: 'http://localhost:5000/api',
+    backendUrl: 'https://nexpo.arkadtlth.se/api',
   },
 }
 
