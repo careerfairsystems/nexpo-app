98bb579583a2da7b06763efcbafaf9013bc2b686
Jonatan Kruse <jonatankruse@hotmail.com>
2022-11-10 20:36:36 +0100

    Co-authored-by: elnaseyer <elnaseyer@users.noreply.github.com>

--- screens/maps/MapScreen.tsx
+++ screens/maps/MapScreen.tsx
@@ -20,7 +20,7 @@ export default function MapScreen({ navigation }: mapNavigation) {
   }
   return (
     <View style={styles.container}>
-      <ImageBackground source={imageSource} style={styles.image}>
+      <ImageBackground resizeMode='contain' source={imageSource} style={styles.image}>
         <ArkadButton style={styles.karbutton} onPress={() => openMap(KarhusetMap)}/> 
         <ArkadButton style={styles.studiebutton} onPress={() => openMap(SC1Map)}/> 
         <ArkadButton style={styles.ebutton} onPress={() => openMap(EMap)}/> 
@@ -37,19 +37,19 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
+    width: '100%',
+    height: '100%',
     justifyContent: 'space-between',
     alignItems: 'center',
-    padding: '2%',
   },
 
   image: {
     flex: 1,
     width: '100%',
-    resizeMode: 'cover',
+    height: '100%',
     justifyContent: "center",
     zIndex: 1,
     opacity: 1,
-    padding: '2%',
     position: "relative",
   },
 
@@ -61,10 +61,9 @@ const styles = StyleSheet.create({
     opacity: 0,
     borderRadius: 100,
     left: '32%',
-    bottom: '54%',
+    bottom: '52%',
     margin: 0,
     zIndex: 1001,
-    backgroundColor: Colors.lightBlue,
   },
 
   studiebutton: {
@@ -78,7 +77,6 @@ const styles = StyleSheet.create({
     left: "0%",
     bottom: "35%",
     zIndex: 1000,
-    //backgroundColor: Colors.lightBlue,
   },
 
   ebutton: {
@@ -89,9 +87,8 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 0,
     left: "59%",
-    bottom: "10%",
+    bottom: "14%",
     borderRadius: 100,
-    backgroundColor: Colors.lightBlue,
   },
 
   etext: {
@@ -102,8 +99,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 0,
     left: "18%",
-    bottom: "10%",
+    bottom: "15%",
     borderRadius: 100,
-    backgroundColor: Colors.lightBlue,
   }
 })
