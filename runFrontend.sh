e4dc5a9071afc671926dc492e60729a4053d47ed
@nils.alexander.hansson <nils.alexander.hansson@gmail.com>
2023-03-19 22:19:30 +0100

    help flag

--- runFrontend.sh
+++ runFrontend.sh
@@ -1,7 +1,3 @@
-# Instruktioner:
-# Första gången: Kör denna fil
-# Sedan: Kör endast npm run start
-
 checkInstalled() {
     if ! [ -x "$(command -v $@)" ]; then
         echo "Error: $@ is not installed." >&2
@@ -11,23 +7,40 @@ checkInstalled() {
     fi
 }
 
+show_help() {
+    echo "Usage: ./runFrondEnd [OPTIONS]"
+    echo "Script for running the frontend"
+    echo ""
+    echo "Options:"
+    echo "-h, --help     Display this help message and exit"
+    echo "-n, --renode   Reinstall node_modules"
+    echo "-c, --recli    Install sharp-cli"
+    echo "-s, --ressl    Add ssl"
+    echo ""
+}
+
 checkInstalled npm
 
-while getopts ":n:c:s" opt; do
+while getopts ":h:n:c:s" opt; do
     case $opt in
-    n)
+    h|help)
+        show_help
+        exit 1
+        ;;
+    n|renode)
         # Reinstall node_modules
         sudo rm -rf node_modules 
         npm install
         ;;
-    c)
+    c|recli)
         # Install sharp-cli  
         npm install -g sharp-cli@^2.1.0
         ;;
-    s)  
+    s|ressl)  
         # Lägg till vid problem med ssl. (opensslErrorStack)
         #Kan leda till att unset NODE_OPTIONS behöver köras innan VSCode kan köras igen
         export NODE_OPTIONS=--openssl-legacy-provider 
+        ;;
     esac
 done
 
