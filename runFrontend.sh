224888b47ad2d432a563173e7e6d2ee89963e5a4
@nils.alexander.hansson <nils.alexander.hansson@gmail.com>
2023-03-19 21:23:50 +0100

    changed sh file to support using flags to solve issues

--- runFrontend.sh
+++ runFrontend.sh
@@ -1,3 +1,7 @@
+# Instruktioner:
+# Första gången: Kör denna fil
+# Sedan: Kör endast npm run start
+
 checkInstalled() {
     if ! [ -x "$(command -v $@)" ]; then
         echo "Error: $@ is not installed." >&2
@@ -9,11 +13,22 @@ checkInstalled() {
 
 checkInstalled npm
 
-sudo rm -rf node_modules 
-npm install
-npm install -g sharp-cli@^2.1.0
-export NODE_OPTIONS=--openssl-legacy-provider 
+while getopts ":n:c:s" opt; do
+    case $opt in
+    n)
+        # Reinstall node_modules
+        sudo rm -rf node_modules 
+        npm install
+        ;;
+    c)
+        # Install sharp-cli  
+        npm install -g sharp-cli@^2.1.0
+        ;;
+    s)  
+        # Lägg till vid problem med ssl. (opensslErrorStack)
+        #Kan leda till att unset NODE_OPTIONS behöver köras innan VSCode kan köras igen
+        export NODE_OPTIONS=--openssl-legacy-provider 
+    esac
+done
+
 npm run start 
-# Första gången: Kör denna fil
-# Sedan: Kör endast npm run start
-# Vid problem med att VSCode inte kan öppnas: kör unset NODE_OPTIONS
