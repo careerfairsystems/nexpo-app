a9c704f18933beec7d0a2b08d892a8cb807d4917
@nils.alexander.hansson <nils.alexander.hansson@gmail.com>
2023-03-19 16:10:18 +0100

    updated frontend bash file

--- runFrontend.sh
+++ runFrontend.sh
@@ -1,5 +1,7 @@
-rm -rf node_modules
+rm -rf node_modules 
 npm install
 npm install -g sharp-cli@^2.1.0
-export NODE_OPTIONS=--openssl-legacy-provider
-npm run start
+export NODE_OPTIONS=--openssl-legacy-provider 
+npm run start 
+# Första gången: Kör denna fil
+# Sedan: Kör endas npm run start
