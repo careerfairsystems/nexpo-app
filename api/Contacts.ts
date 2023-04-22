9b38d7118a64f744a1fca6219db80f0c3efc9bc4
Stronkness <andrefrisk98@gmail.com>
2023-04-22 23:25:31 +0200

    Made contacts visible and add CSS

--- api/Contacts.ts
+++ api/Contacts.ts
@@ -1,19 +1,17 @@
 import { getAuth } from "./_HttpHelpers";
 
 export interface Contact {
-    Id: number,
-    FirstName: string,
-    LastName: string,
-    RoleInArkad: string,
-    Email: string,
-    PhoneNumber: string,
-  }
+	id: number;
+	firstName: string;
+	lastName: string;
+	roleInArkad: string;
+	email: string;
+	phoneNumber: string;
+}
 
 export const contacts = async (): Promise<Contact[]> => {
-  const response = await getAuth("/contacts");
-  const json = await response.json();
-  const contacts = json as Contact[];
-  console.log(contacts)
-  return contacts;
+	const response = await getAuth("/contacts");
+	const json = await response.json();
+	const contacts = json as Contact[];
+	return contacts;
 };
-
--- components/profileScreen/ContactsPG.tsx
+++ components/profileScreen/ContactsPG.tsx
@@ -1,37 +1,66 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text } from "../Themed";
 import Colors from "constants/Colors";
 import { ActivityIndicator, ScrollView, StyleSheet } from "react-native";
 import { ArkadText } from "components/StyledText";
 import { API } from "api/API";
-
-async function testapi() {
-    console.log("test");
-    const test = await API.contacts.contacts();
-    console.log(test)
-}
+import { Contact } from "api/Contacts";
 
 export default function Contacts() {
-    return (
-        <ScrollView style={styles.container}>
-            <ArkadText text={"Contacts"} style={styles.header}/>
-            <button onClick={testapi}>Click me senpai</button>
-        </ScrollView>
-    )
+	const [contacts, setContacts] = React.useState<Contact[]>();
+
+	useEffect(() => {
+		fetch_contacts();
+	}, []);
+
+	async function fetch_contacts() {
+		const contacts = await API.contacts.contacts();
+		setContacts(contacts);
+	}
+
+	return (
+		<ScrollView style={styles.container}>
+			<ArkadText text={"Contacts"} style={styles.header} />
+			{contacts?.reverse().map((contact) => (
+				<View key={contact.id} style={styles.contactContainer}>
+					<ArkadText text={contact.roleInArkad} style={styles.role} />
+					<ArkadText
+						text={contact.firstName + " " + contact.lastName}
+						style={styles.text}
+					/>
+					<ArkadText text={contact.email} style={styles.text} />
+					<ArkadText text={contact.phoneNumber} style={styles.text} />
+				</View>
+			))}
+		</ScrollView>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: {
-      display: "flex",
-      paddingVertical: 24,
-      backgroundColor: Colors.white,
-    },
-    header: {
-        width: "100%",
-        textAlign: "center",
-        fontSize: 24,
-        color: Colors.arkadNavy,
-        justifyContent: "center",
-      },
-  });
-  
+	container: {
+		paddingVertical: 24,
+		backgroundColor: Colors.white,
+	},
+	header: {
+		width: "100%",
+		textAlign: "center",
+		fontSize: 30,
+		color: Colors.arkadNavy,
+	},
+	role: {
+		fontWeight: "bold",
+		color: Colors.arkadOrange,
+		fontSize: 24,
+		paddingTop: 10,
+	},
+	text: {
+		color: Colors.arkadNavy,
+		fontSize: 18,
+	},
+	contactContainer: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		textAlign: "left",
+	},
+});
