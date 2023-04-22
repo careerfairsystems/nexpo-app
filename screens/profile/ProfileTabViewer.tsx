183a3e00508e57f2c618b4dc04406e036a59aba8
Stronkness <andrefrisk98@gmail.com>
2023-04-23 00:15:57 +0200

    Change design of bar: TabView

--- screens/profile/ProfileTabViewer.tsx
+++ screens/profile/ProfileTabViewer.tsx
@@ -1,39 +1,52 @@
 import React from "react";
-import { View, Text } from "components/Themed";
-import { useWindowDimensions } from 'react-native';
-import { TabView, SceneMap } from 'react-native-tab-view';
-import { ActivityIndicator, ScrollView, StyleSheet } from "react-native";
+import { useWindowDimensions } from "react-native";
+import { TabView, SceneMap, TabBar } from "react-native-tab-view";
+import { StyleSheet } from "react-native";
 import Colors from "constants/Colors";
 
-function ProfileTabViewer(props: {profile: any, contacts: any}) {
-    const layout = useWindowDimensions();
+function ProfileTabViewer(props: { profile: any; contacts: any }) {
+	const layout = useWindowDimensions();
 
-    const [index, setIndex] = React.useState(0);
-    const [routes] = React.useState([
-      { key: 'first', title: 'Profile' },
-      { key: 'second', title: 'Contact List' },
-    ]);
-  
-    const renderScene = SceneMap({
-      first: props.profile,
-      second: props.contacts,
-    });
-  
-    return (
-        <TabView
-          style={styles.tabViewer}
-          navigationState={{ index, routes }}
-          renderScene={renderScene}
-          onIndexChange={setIndex}
-          initialLayout={{ width: layout.width }}
-        />
-      );
-    }
+	const [index, setIndex] = React.useState(0);
+	const [routes] = React.useState([
+		{ key: "first", title: "Profile" },
+		{ key: "second", title: "Contact List" },
+	]);
 
-  export default ProfileTabViewer;
+	const renderScene = SceneMap({
+		first: props.profile,
+		second: props.contacts,
+	});
 
-  const styles = StyleSheet.create({
-    tabViewer: {
-      backgroundColor: Colors.white,
-    },
-  });
+	const renderTabBar = (props: any) => (
+		<TabBar
+			{...props}
+			indicatorStyle={{ backgroundColor: Colors.arkadOrange }}
+			style={{ backgroundColor: Colors.arkadNavy }}
+			labelStyle={{
+				fontWeight: "bold",
+				fontFamily: "main-font",
+				fontSize: 20,
+			}}
+		/>
+	);
+
+	return (
+		<TabView
+			style={styles.tabViewer}
+			navigationState={{ index, routes }}
+			renderScene={renderScene}
+			onIndexChange={setIndex}
+			initialLayout={{ width: layout.width }}
+			renderTabBar={renderTabBar}
+		/>
+	);
+}
+
+export default ProfileTabViewer;
+
+const styles = StyleSheet.create({
+	tabViewer: {
+		backgroundColor: Colors.white,
+	},
+});
