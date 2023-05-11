65c77802b605d53b100d0b9d4020a1e327cfede8
Stronkness <andrefrisk98@gmail.com>
2023-05-11 19:27:47 +0200

    Temporarly disabled splash screen when on DEV

--- screens/profile/ProfileTabViewer.tsx
+++ screens/profile/ProfileTabViewer.tsx
@@ -1,35 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useWindowDimensions } from "react-native";
 import { TabView, SceneMap, TabBar } from "react-native-tab-view";
 import { StyleSheet } from "react-native";
 import Colors from "constants/Colors";
 
-function ProfileTabViewer(props: { profile: any; contacts: any; messages: any; admin?: any }) {
+function ProfileTabViewer(props: {
+  profile: any;
+  contacts: any;
+  messages: any;
+  admin?: any;
+}) {
   const layout = useWindowDimensions();
 
   const [index, setIndex] = React.useState(0);
 
-  const [routes] = props.admin ? React.useState([
-    { key: "first", title: "Profile" },
-    { key: "second", title: "Contact List" },
-    { key: "third", title: "Messages" },
-    { key: "fourth", title: "Admin" },
-  ]) : React.useState([
-    { key: "first", title: "Profile" },
-    { key: "second", title: "Contact List" },
-    { key: "third", title: "Messages" },
-  ])
+  const [routes] = props.admin
+    ? React.useState([
+        { key: "first", title: "Profile" },
+        { key: "second", title: "Contact List" },
+        { key: "third", title: "Messages" },
+        { key: "fourth", title: "Admin" },
+      ])
+    : React.useState([
+        { key: "first", title: "Profile" },
+        { key: "second", title: "Contact List" },
+        { key: "third", title: "Messages" },
+      ]);
 
-  const renderScene = props.admin ? SceneMap({
-    first: props.profile,
-    second: props.contacts,
-    third: props.messages,
-    fourth: props.admin,
-  }) : SceneMap({
-    first: props.profile,
-    second: props.contacts,
-    third: props.messages,
-  })
+  const renderScene = props.admin
+    ? SceneMap({
+        first: props.profile,
+        second: props.contacts,
+        third: props.messages,
+        fourth: props.admin,
+      })
+    : SceneMap({
+        first: props.profile,
+        second: props.contacts,
+        third: props.messages,
+      });
+
+  useEffect(() => {});
 
   const renderTabBar = (props: any) => (
     <TabBar
