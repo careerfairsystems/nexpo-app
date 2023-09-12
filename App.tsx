2f6aebef95ee14d48c50eae6a9d5db7e2ab2058e
Stronkness <andrefrisk98@gmail.com>
2023-09-12 21:49:38 +0200

    Firebase: Push Notifications done
    This cannot be tested unless on a real device sadly. However every
    component and functionality exist that it should work, atleast for
    Android. If it works for Android but not Apple the certificates might be
    made wrong. Check the article again if something doesnt work

--- App.tsx
+++ App.tsx
@@ -5,7 +5,57 @@ import AppLoader from "./screens/AppLoader";
 import Navigation from "./navigation";
 import Toast from "react-native-toast-message";
 
+import messaging from "@react-native-firebase/messaging";
+import { useEffect } from "react";
+import { Alert, AppRegistry } from "react-native";
+
+// Good article about FCM:
+// https://medium.com/@arashfallahi1989/how-to-integrate-firebase-push-notification-in-react-native-expo-bd5cc694f181
+
 export default function App() {
+  // Register background handler
+  messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+    console.log("Message handled in the background!", remoteMessage);
+  });
+  AppRegistry.registerComponent("app", () => App);
+
+  // messaging()
+  // .subscribeToTopic('weather')
+  // .then(() => console.log('Subscribed to topic!'));
+
+  // messaging()
+  // .unsubscribeFromTopic('weather')
+  // .then(() => console.log('Unsubscribed fom the topic!'));
+
+  useEffect(() => {
+    async function requestUserPermission() {
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+      if (enabled) {
+        console.log("Authorization status:", authStatus);
+
+        // Get the token
+        const fcmToken = await messaging().getToken();
+        if (fcmToken) {
+          console.log("Your Firebase Cloud Messaging token is:", fcmToken);
+        } else {
+          console.log("Failed to get FCM token");
+        }
+      }
+    }
+
+    requestUserPermission();
+
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      Alert.alert("A new FCM message arrived!");
+      console.log(JSON.stringify(remoteMessage));
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <AppLoader>
       <SafeAreaProvider>
@@ -15,6 +65,5 @@ export default function App() {
 
       <Toast />
     </AppLoader>
-    
   );
 }
