bc17ec6771af2593a05ce21191a4da40634fc89c
Stronkness <andrefrisk98@gmail.com>
2023-09-18 18:35:23 +0200

    Add reverse API for jwt token

--- api/SSO.ts
+++ api/SSO.ts
@@ -1,15 +1,49 @@
-import { get } from "./_HttpHelpers"
+import { get } from "./_HttpHelpers";
 import { API } from "api/API";
 
-
 export const InitiateSSO = async (setSignedIn: (value: boolean) => void): Promise<void> => {
-    const response = await get("/saml/initiatesinglesignon");
-    const split = response.headers.get('Location')?.split('https://www.nexpo.arkadtlth.se/api/Saml/');
-    const jwt = split ? split[1] : null;
+  const response = await get("/saml/initiatesinglesignon");
 
-    const success = await API.auth.SSOLogin(jwt);
+  // Initialize WebSocket connection
+  return new Promise((resolve, reject) => {
+    const ws = new WebSocket('wss://nexpo.arkadtlth.se:8080'); // Replace with your WebSocket URL
 
-    if (success) {
-        setSignedIn(true);
-    }
-}
+    ws.onopen = () => {
+      // Connection opened
+      ws.send('Hello from Frontend'); // Send a message to initiate the process if needed
+    };
+
+    ws.onmessage = async (e) => {
+      // Receive message from backend
+      const message = JSON.parse(e.data);
+
+      if (message.payload) {
+        const jwtToken = message.payload;
+
+        // Use the received JWT token for SSO login
+        const success = await API.auth.SSOLogin(jwtToken);
+
+        if (success) {
+          setSignedIn(true);
+        }
+
+        // Close the WebSocket connection
+        ws.close();
+
+        // Resolve the promise
+        resolve();
+      }
+    };
+
+    ws.onerror = (e) => {
+      // An error occurred
+      console.log(e);
+      reject(e);
+    };
+
+    ws.onclose = (e) => {
+      // Connection closed
+      console.log(e.code, e.reason);
+    };
+  });
+};
