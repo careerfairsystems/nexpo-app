81cd678a9bed239f9b2af461bf4ed23c64545599
Stronkness <andrefrisk98@gmail.com>
2023-09-20 08:46:09 +0200

    Make QuestionsTab formatted with Prettier

--- components/profileScreen/QuestionTab.tsx
+++ components/profileScreen/QuestionTab.tsx
@@ -8,30 +8,43 @@ import { getMe } from "api/Users";
 export default function QuestionTab() {
   const [title, setTitle] = useState("");
   const [text, setText] = useState("");
-  
+
   const sendEmail = async () => {
     const user = await getMe();
-    const recipient = ['plarkad+29zvgxoon7cl0wrxm5tb@boards.trello.com']; // List of recipient email addresses
+    const recipient = ["plarkad+29zvgxoon7cl0wrxm5tb@boards.trello.com"]; // List of recipient email addresses
     const subject = title;
-    
+
     const sender = "From: " + user.email;
-    const body = "Please do not remove the From and Message tags!\n\n" + sender + "\Message: " + text;
+    const body =
+      "Please do not remove the From and Message tags!\n\n" +
+      sender +
+      "Message: " +
+      text;
 
     const emailUrl = `mailto:${recipient}?subject=${encodeURIComponent(
-        subject
-      )}&body=${encodeURIComponent(body)}`;
+      subject
+    )}&body=${encodeURIComponent(body)}`;
 
     Linking.openURL(emailUrl)
-    .then(() => console.log('Email opened'))
-    .catch((error) => console.error('Error opening email:', error));
+      .then(() => console.log("Email opened"))
+      .catch((error) => console.error("Error opening email:", error));
 
     setText("");
     setTitle("");
   };
 
   return (
-    <ScrollView style={styles.container} showsVerticalScrollIndicator={false} contentContainerStyle={{ alignItems: "center" }}>
-      <ArkadText text={"If you have any questions please\nsend us an email and we will\nrespond as fast as we can!"} style={styles.header} />
+    <ScrollView
+      style={styles.container}
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{ alignItems: "center" }}
+    >
+      <ArkadText
+        text={
+          "If you have any questions please\nsend us an email and we will\nrespond as fast as we can!"
+        }
+        style={styles.header}
+      />
       <TextInput
         style={styles.titleInput}
         onChangeText={setTitle}
@@ -40,17 +53,17 @@ export default function QuestionTab() {
         placeholderTextColor={Colors.lightGray}
         multiline={false}
         textAlign="center"
-			/>
-			<TextInput
-				style={styles.textInput}
-				onChangeText={setText}
-				value={text}
-				placeholder={"Message to send..."}
-				placeholderTextColor={Colors.lightGray}
-				multiline={true}
-				textAlignVertical="top"
-				numberOfLines={10}
-			/>
+      />
+      <TextInput
+        style={styles.textInput}
+        onChangeText={setText}
+        value={text}
+        placeholder={"Message to send..."}
+        placeholderTextColor={Colors.lightGray}
+        multiline={true}
+        textAlignVertical="top"
+        numberOfLines={10}
+      />
       <ArkadButton onPress={sendEmail} style={styles.buttonContainer1}>
         <ArkadText text="Send" style={styles.buttonText} />
       </ArkadButton>
@@ -68,22 +81,22 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 30,
     color: Colors.white,
-    marginBottom: 15
+    marginBottom: 15,
   },
   textInput: {
-		height: "60",
-		margin: 0,
-		borderColor: Colors.white,
-		color: Colors.white,
-		borderRadius: 7,
-		borderWidth: 2,
-		fontSize: 20,
-		fontFamily: "main-font-bold",
-		padding: 10,
-		width: "90%",
-        backgroundColor: Colors.arkadNavy
-	},
-titleInput: {
+    height: "60",
+    margin: 0,
+    borderColor: Colors.white,
+    color: Colors.white,
+    borderRadius: 7,
+    borderWidth: 2,
+    fontSize: 20,
+    fontFamily: "main-font-bold",
+    padding: 10,
+    width: "90%",
+    backgroundColor: Colors.arkadNavy,
+  },
+  titleInput: {
     height: "20",
     borderColor: Colors.white,
     color: Colors.white,
@@ -95,17 +108,17 @@ titleInput: {
     margin: 0,
     marginBottom: 12,
     width: "90%",
-    backgroundColor: Colors.arkadNavy
+    backgroundColor: Colors.arkadNavy,
   },
   buttonText: {
     padding: "1%",
     alignItems: "center",
     fontSize: 18,
-    },
-    buttonContainer1: {
-        alignSelf: "center",
-        padding: "4%",
-        marginBottom: "2%",
-        width: "45%",
-    },
+  },
+  buttonContainer1: {
+    alignSelf: "center",
+    padding: "4%",
+    marginBottom: "2%",
+    width: "45%",
+  },
 });
