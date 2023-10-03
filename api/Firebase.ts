b0d60980336efae94f22449062d0b557f252c7a9
= <emil.ronkainen@gmail.com>
2023-10-03 12:26:34 +0200

    API: handle edge cases where response is required
    Change api calls to return manually crafted Response objects
    where short-circuiting is not a valid approach.

--- api/Firebase.ts
+++ api/Firebase.ts
@@ -7,5 +7,9 @@ export type RegisterUserDTO = {
 
 export const registerFirebase = async (dto: RegisterUserDTO): Promise<Response> => {
   const response = await post("/register", dto);
+
+  if (!response)
+    return {status: 400, statusText: "Something went wrong"} as Response;
+  
   return response;
 };
