b4f1506f148edfb85f56b9c6c9f9eaf007aec4ae
Stronkness <andrefrisk98@gmail.com>
2023-10-18 13:43:15 +0200

    iOS: Testing home indicator bar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
-import { StatusBar, Platform } from "react-native";
+import { StatusBar, Platform, SafeAreaView } from "react-native";
+import React, { Component, Fragment } from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
@@ -13,6 +14,7 @@ import { API } from "api/API";
 import { RegisterUserDTO } from "api/Firebase";
 
 import * as Linking from "expo-linking";
+import Colors from "constants/Colors";
 
 const prefix = Linking.createURL("/");
 
@@ -20,68 +22,73 @@ const prefix = Linking.createURL("/");
 //https://medium.com/@arashfallahi1989/how-to-integrate-firebase-push-notification-in-react-native-expo-bd5cc694f181
 
 export default function App() {
-  // Register background handler
-  messaging().setBackgroundMessageHandler(async (remoteMessage: any) => {
-    console.log("Message handled in the background!", remoteMessage);
-  });
-  AppRegistry.registerComponent("se.arkadtlth.nexpo", () => App);
+  if (!__DEV__) {
+    // Register background handler
+    messaging().setBackgroundMessageHandler(async (remoteMessage: any) => {
+      console.log("Message handled in the background!", remoteMessage);
+    });
+    AppRegistry.registerComponent("se.arkadtlth.nexpo", () => App);
 
-  const linking = {
-    prefixes: [prefix],
-  };
+    const linking = {
+      prefixes: [prefix],
+    };
 
-  messaging()
-    .subscribeToTopic("weather")
-    .then(() => console.log("Subscribed to topic!"));
+    messaging()
+      .subscribeToTopic("weather")
+      .then(() => console.log("Subscribed to topic!"));
 
-  messaging()
-    .unsubscribeFromTopic("weather")
-    .then(() => console.log("Unsubscribed fom the topic!"));
+    messaging()
+      .unsubscribeFromTopic("weather")
+      .then(() => console.log("Unsubscribed fom the topic!"));
 
-  useEffect(() => {
-    async function requestUserPermission() {
-      const authStatus = await messaging().requestPermission();
-      const enabled =
-        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+    useEffect(() => {
+      async function requestUserPermission() {
+        const authStatus = await messaging().requestPermission();
+        const enabled =
+          authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+          authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
-      if (enabled) {
-        console.log("Authorization status:", authStatus);
+        if (enabled) {
+          console.log("Authorization status:", authStatus);
 
-        // Get the token
-        const fcmToken = await messaging().getToken();
-        if (fcmToken) {
-          console.log("Your Firebase Cloud Messaging token is:", fcmToken);
-          const register: RegisterUserDTO = {
-            token: fcmToken,
-            topic: "all",
-          };
+          // Get the token
+          const fcmToken = await messaging().getToken();
+          if (fcmToken) {
+            console.log("Your Firebase Cloud Messaging token is:", fcmToken);
+            const register: RegisterUserDTO = {
+              token: fcmToken,
+              topic: "all",
+            };
 
-          const response = API.firebase.registerFirebase(register);
-          console.log("Firebase: ", response);
-        } else {
-          console.log("Failed to get FCM token");
+            const response = API.firebase.registerFirebase(register);
+            console.log("Firebase: ", response);
+          } else {
+            console.log("Failed to get FCM token");
+          }
         }
       }
-    }
 
-    requestUserPermission();
+      requestUserPermission();
 
-    const unsubscribe = messaging().onMessage(async (remoteMessage: any) => {
-      Alert.alert("A new FCM message arrived!");
-      console.log(JSON.stringify(remoteMessage));
-    });
-    return unsubscribe;
-  }, []);
+      const unsubscribe = messaging().onMessage(async (remoteMessage: any) => {
+        Alert.alert("A new FCM message arrived!");
+        console.log(JSON.stringify(remoteMessage));
+      });
+      return unsubscribe;
+    }, []);
+  }
 
   return (
     <AppLoader>
-      <SafeAreaProvider>
-        <Navigation />
-        <ExpoStatusBar style="light" />
-      </SafeAreaProvider>
+      <Fragment>
+        <SafeAreaProvider>
+          <Navigation />
+          <ExpoStatusBar style="light" />
+        </SafeAreaProvider>
 
-      <Toast />
+        <SafeAreaView style={{ flex: 0, backgroundColor: Colors.arkadNavy }} />
+        <Toast />
+      </Fragment>
     </AppLoader>
   );
 }
