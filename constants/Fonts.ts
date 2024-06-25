347b79403df28453b24b99372794988847f8fe2e
Lukas K <lukaskuhlin@gmail.com>
2024-06-25 08:48:49 +0200

    fonttttt

--- constants/Fonts.ts
+++ constants/Fonts.ts
@@ -1,1 +1,12 @@
-export default {};
+import { Platform } from "react-native";
+
+export default {
+    MyriadProRegular: Platform.select({
+        ios: 'MyriadPro-Regular',
+        android: 'MyriadPro-Regular', // On Android, you typically don't need the file extension
+      }),
+      MyriadProBold: Platform.select({
+        ios: 'MyriadPro-Bold',
+        android: 'MyriadPro-Bold',
+      }),
+};
