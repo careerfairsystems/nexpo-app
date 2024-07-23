29a5f1516a028b39cad1225bdfcf2fe51009852a
Tristan Farkas <tr1508fa-s@student.lu.se>
2024-07-23 18:44:57 +0200

    updated

--- app.config.ts
+++ app.config.ts
@@ -15,70 +15,68 @@ export default {
     fallbackToCacheTimeout: 0,
   },
   assetBundlePatterns: ["**/*"],
-  "expo": { 
+  expo: {
     ios: {
-    bundleIdentifier: "se.arkadtlth.nexpo",
+      bundleIdentifier: "se.arkadtlth.nexpo",
 
-    googleServicesFile: "./GoogleService-Info.plist",
-    scheme: "se.arkadtlth.nexpo",
-    target: 'nexpoapp',
-    buildNumber: "2.0.0",
-    plugins: [
-      "@react-native-firebase/app"
-    ],
-    supportsTablet: true,
-    infoPlist: {
-      NSCameraUsageDescription: "Camera access is needed for the QR-code scanning functionality",
-      NSPhotoLibraryUsageDescription:
-        "Photo library access is needed to choose a profile picture to upload",
-      UIBackgroundModes: [
-        "fetch",
-        "remote-notification"
+      googleServicesFile: "./GoogleService-Info.plist",
+      scheme: "se.arkadtlth.nexpo",
+      target: "nexpoapp",
+      buildNumber: "2.0.0",
+      plugins: [
+        "@react-native-firebase/app",
+        "expo-build-properties",
+        {
+          ios: {
+            useFrameworks: "static",
+          },
+        },
       ],
+      supportsTablet: true,
+      infoPlist: {
+        NSCameraUsageDescription:
+          "Camera access is needed for the QR-code scanning functionality",
+        NSPhotoLibraryUsageDescription:
+          "Photo library access is needed to choose a profile picture to upload",
+        UIBackgroundModes: ["fetch", "remote-notification"],
+      },
     },
-  },
-  android: {
-    package: "se.arkadtlth.nexpo",
-    googleServicesFile: "./google-services.json",
-    buildNumber: "2.0.0",
-    displayName: "ARKAD",
-    appName: "ARKAD",
-    "env": {
-      "ANDROID_HOME": "/Users/victormikkelsen/Library/Android/sdk"
+    android: {
+      package: "se.arkadtlth.nexpo",
+      googleServicesFile: "./google-services.json",
+      buildNumber: "2.0.0",
+      displayName: "ARKAD",
+      appName: "ARKAD",
+      env: {
+        ANDROID_HOME: "/Users/victormikkelsen/Library/Android/sdk",
+      },
+      versionCode: 177,
+      scheme: "se.arkadtlth.nexpo",
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/android_icon.png",
+        backgroundColor: "#F66628",
+      },
+      permissions: ["CAMERA"],
     },
-    versionCode: 177,
-    scheme: "se.arkadtlth.nexpo",
-    adaptiveIcon: {
-      foregroundImage: "./assets/images/android_icon.png",
-      backgroundColor: "#F66628",
+    plugins: ["@react-native-firebase/app"],
+    web: {
+      favicon: "./assets/images/favicon.png",
     },
-    permissions: ["CAMERA"],
-  },
-  plugins: [
-    "@react-native-firebase/app"
-  ],
-  web: {
-    favicon: "./assets/images/favicon.png",
-  },
-  extra: {
-    isDev: process.env.NODE_ENV === "false",
-    isProd: process.env.NODE_ENV !== "true",
-    backendUrl: 'https://nexpo.arkadtlth.se/api',
-    //backendUrl: "http://localhost:5000/api",
-    "eas": {
-      target: 'nexpoapp',
-      "projectId": "736e5c8b-3245-43fc-9803-293e803f584e"
-     }
+    extra: {
+      isDev: process.env.NODE_ENV === "false",
+      isProd: process.env.NODE_ENV !== "true",
+      backendUrl: "https://nexpo.arkadtlth.se/api",
+      //backendUrl: "http://localhost:5000/api",
+      eas: {
+        target: "nexpoapp",
+        projectId: "736e5c8b-3245-43fc-9803-293e803f584e",
+      },
     },
     name: "ARKAD",
     slug: "nexpo-app",
     version: "1.0.0",
     sdkVersion: "48.0.0",
     googleServicesFile: "./google-services.json",
-    platforms: [
-      "ios",
-      "android",
-      "web"
-    ]
-  }
-  };
+    platforms: ["ios", "android", "web"],
+  },
+};
